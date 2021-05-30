6f96edb4cb4b6857f4795a07a3dc38eba4db846d
Mykel Valadez <valadezmykel@gmail.com>
2021-05-29 17:40:37 -0700

    gonna mess with somethings css won't update

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -6,21 +6,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebUI.Models;
+using BuisnessLogic;
 
 namespace WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        private BLogic _BL;
+        // public HomeController(BLogic BL)
+        // {
+        //     _BL = BL;
+        // }
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BLogic BL)
         {
             _logger = logger;
+            _BL = BL;
         }
 
         public ActionResult Index(int id)
         {
             return View(new CustomerVM(id));
+        }
+
+        public ActionResult GetLocations(){
+            List<LocationVM> stores = new List<LocationVM>();
+            _BL.GetAllStores().ForEach(i => stores.Add(new LocationVM(i)));
+            return View(stores);
         }
 
         public IActionResult Privacy()
