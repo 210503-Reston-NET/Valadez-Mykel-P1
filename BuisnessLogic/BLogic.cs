485eec0bfd832d271a0ac805c4c1fa659752beed
Mykel Valadez <valadezmykel@gmail.com>
2021-06-02 09:37:40 -0700

    small css change to body margin top

--- BuisnessLogic/BLogic.cs
+++ BuisnessLogic/BLogic.cs
@@ -15,7 +15,12 @@ namespace BuisnessLogic
         {
             _DB = DB;
         }
-
+        /// <summary>
+        /// This will check if a user exists in the DB
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns>Customer ID</returns>
         public int CheckUserCredentials(string email, string password){
             Log.Information("retriving user");
             Log.Debug("DebugLog");
@@ -26,29 +31,56 @@ namespace BuisnessLogic
         public void FindUser(string name){
             Customer cust = _DB.FindCustomer(name);
         }
+        /// <summary>
+        /// Finds a Customer by id from the db
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns>A Customer</returns>
         public Customer FindUser(int customerId){
             return _DB.FindCustomer(customerId);
         }
-
+        /// <summary>
+        /// This finds 1 order and order detail by id to be used to create an OrderVM
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns>Tuple of order and orderdetail</returns>
         public Tuple<Order, OrderDetail> CheckOrder(int orderId){
             return _DB.ViewOrder(orderId);
         }
-
+        /// <summary>
+        /// Adds a new user to the DB
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns>Customer ID</returns>
         public int AddNewUser(string name, string email, string password){
             _CustID = _DB.AddUser(name, email, password);
             if(!(_CustID > 0)) throw new Exception("Unable to verify user creation");
             return _CustID;
         }
-
+        /// <summary>
+        /// Finds a Location by name from the db
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>A Location</returns>
         public Location FindLocation(string name){
             return _DB.FIndLocation(name);
         }
-
+        /// <summary>
+        /// Finds a Location by id from the db
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A location</returns>
         public Location FindLocationById(int id)
         {
             return _DB.FindLocation(id);
         }
-
+        /// <summary>
+        /// Asks DB to find all Inventories with the same store id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>List of LocationProductInventories</returns>
         public List<LocationProductInventory> ViewInventory(int id)
         {
             return _DB.ViewInventory(id);
